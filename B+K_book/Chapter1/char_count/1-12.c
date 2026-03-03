c32fee1c253028949a52711f0aaf6ade990a64f9
hamiyee <hmj3632@163.com>
2026-03-03 10:18:52 +0800

    optimize dirs

---
+++ B+K_book/Chapter1/char_count/1-12.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main(int argc, char const *argv[])
+{
+    int c, number_line, number_word, number_char, state;
+    // number_line = number_word = number_char = 0;
+    state = OUT;
+
+    while ((c = getchar()) != EOF)
+    {
+        // ++number_char;
+
+        // if (c == '\n')
+        // {
+        //     ++number_line;
+        // }
+        if (c == ' ' || c == '\n' || c == '\t'||c=='\r')
+        {
+            // state = OUT;
+            c = '\n';
+        }
+        putchar(c);
+
+        // else if (state == OUT)
+        // {
+        //     state = IN;
+        //     ++number_word;
+        // }
+    }
+
+    // printf("%d %d %d\n", number_line, number_word, number_char);
+
+    return 0;
+}
